f6b11566838dee2bee5f91690cb2abf4851cb79f
Phantola <phantoladev@gmail.com>
2024-06-29 22:49:34 +0900

    Add redirect and library(@nestjs/serve-static)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = 3001;
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
